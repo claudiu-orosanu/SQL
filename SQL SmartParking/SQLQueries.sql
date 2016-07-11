aabc6794fad3f0fb69e702a6ad10aed20037b7a4
claudiu-orosanu <orosanu.claudiu@gmail.com>
2016-07-11 19:57:27 +0300

    Update SQLQueries.sql

--- SQL SmartParking/SQLQueries.sql
+++ SQL SmartParking/SQLQueries.sql
@@ -17,29 +17,47 @@ AND DATEPART(month,DataSosire) = 7
 
 SELECT SUM((CASE WHEN Durata > 2 THEN (Durata-2)*2 ELSE 0 END)) [Incasari 5 iulie 2016]
 FROM Vizita
-WHERE DataSosire BETWEEN '2016-07-05 00:00:00.00' AND '2016-07-05 23:59:59.999'
+WHERE DATEPART(year,DataSosire) = 2016
+AND DATEPART(month,DataSosire) = 7
+AND DATEPART(day,DataSosire) = 5
 
 
 
 --numar mediu de masini in fiecare ora in ziua de 5 iulie 2016
+DECLARE @year INT
+DECLARE @month INT
+DECLARE @day INT
+SET @year = 2016
+SET @month = 7
+SET @day = 5
+
 SELECT s.[Hour], COUNT(*) AS Vizite
 FROM (SELECT DATEPART(hour,DataSosire) AS Hour
 	  FROM Vizita 
-	  WHERE DataSosire BETWEEN '2016-07-05 00:00:00.000' AND '2016-07-05 23:59:59.999')s
-GROUP BY s.[Hour]
+	  WHERE DATEPART(year,DataSosire) = @year
+		AND DATEPART(month,DataSosire) = @month
+		AND DATEPART(day,DataSosire) = @day) s
+GROUP BY s.[Hour] 
 
 
 --numar mediu de masini pe zi pe parcursul anului 2016
 SELECT COUNT(*)/365.00
 FROM Vizita
-WHERE DataSosire BETWEEN '2016-01-01 00:00:00.000' AND '2016-12-31 23:59:59.999'
+WHERE DATEPART(year,DataSosire) = 2016
+
 
 --numar mediu de masini pe zi parcursul saptamanii 1-7 iulie 2016
+DECLARE @d1 DATETIME
+DECLARE @d2 DATETIME
+SET @d1 = '2016-07-01 00:00:00.000'
+SET @d2 = '2016-07-07 23:59:59.999'
+
 SELECT COUNT(*)/7.00
 FROM Vizita
-WHERE DataSosire BETWEEN '2016-07-01 00:00:00.000' AND '2016-07-07 23:59:59.999'
+WHERE DataSosire >= @d1 AND DataSosire <= @d2
 
---cel mai fidel client
+
+------cel mai fidel client-------
 SELECT m.ID,m.NumarInmatriculare,SUM(v.Durata) [TimpTotal],COUNT(*) as Vizite
 FROM Vizita v
 INNER JOIN Masina m
@@ -52,6 +70,7 @@ ORDER BY TimpTotal DESC
 SELECT AVG(Durata)
 FROM Vizita
 
+
 --- care este a fost cea mai aglomerata zi din anul 2016 in ceea ce priveste parcarea
 SELECT DATEPART(day,DataSosire) as Zi,DATEPART(month,DataSosire) as Luna, COUNT(*) [Numar masini parcate]
 FROM Vizita
@@ -59,12 +78,21 @@ WHERE DATEPART(year,DataSosire) = 2016
 GROUP BY DATEPART(day,DataSosire),DATEPART(month,DataSosire)
 ORDER BY COUNT(*) DESC
 
+
 ----- care este cea mai plina zi din saptamana 1-7 iulie 2016
+DECLARE @d1 DATETIME
+DECLARE @d2 DATETIME
+SET @d1 = '2016-07-01 00:00:00.000'
+SET @d2 = '2016-07-07 23:59:59.999'
+
 SELECT DATENAME(dw,DataSosire),COUNT(*)
 FROM Vizita
-WHERE DataSosire BETWEEN '2016-07-01 00:00:00.000' AND '2016-07-07 23:59:59.999'
+WHERE DataSosire >= @d1 AND DataSosire <= @d2
 GROUP BY DATENAME(dw,DataSosire)
 ORDER BY COUNT(*) DESC
+
+
+
 
 
 ------ care este intervalul orar cu cei mai multi utilizatori ai parcarii (cand sunt cele mai multe masini)
